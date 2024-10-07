0703c7b2411b456c323cad7064e0157403dcd56d
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-10-07 11:57:54 +0200

    skills design update

--- portfolio-react/src/components/Skills.jsx
+++ portfolio-react/src/components/Skills.jsx
@@ -5,8 +5,15 @@ import { SKILLS } from "../utils/data";
 const Skills = () => {
   return (
     <section className="max-w-screen-xl px-6 mx-auto pb-20" id="skills">
-      <div className="bg-gradient-to-br from-fuchsia-950 to-slate-950 rounded-lg border border-fuchsia-500 p-4 md:p-8">
-        <h5 className="text-xl font-medium mb-5">Skills</h5>
+      <div className="bg-gradient-to-br from-fuchsia-950 to-slate-950 rounded-lg border border-neutral-950 p-4 md:p-8">
+        <h5
+          className="text-xl font-medium mb-5"
+          style={{
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.2)",
+          }}
+        >
+          Skills
+        </h5>
 
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           {SKILLS.map((skill) => (
@@ -26,10 +33,10 @@ const Skills = () => {
 
 const SkillsCard = ({ icon: Icon, iconProps, title, comment }) => {
   return (
-    <div className="bg-slate-900 rounded border borderfuchsia-500 p-5">
+    <div className="bg-slate-950 rounded border border-slate-50 p-5">
       <div className="flex items-center justify-between mb-5">
         <p className="text-base">{title}</p>
-        <Icon className="text-primary text-3xl" />
+        <Icon className="text-white text-3xl" />
       </div>
 
       <p className="text-xs font-light leading-5 opacity-80">{comment}</p>
